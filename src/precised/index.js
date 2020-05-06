903601d6b14ce657e8ff233c8de14bdb1ca41592
oyershov <oyershov@heliostech.fr>
2020-05-06 21:41:16 +0300

    Add float separator parameter to precising method

--- src/precised/index.js
+++ src/precised/index.js
@@ -1,12 +1,12 @@
 import { as_integer, format, addTrailingZeros } from './helpers';
 
-/* Should be as params */
-const DECIMAL_SEPARATOR = '.';
+/* Default constants */
+const DEC_SEP = '.'; // decimal separator
 
 export class Precised {
     constructor(num = 0) {
-        this.internal = String(+num);
-        this.as_int = as_integer(this.internal, DECIMAL_SEPARATOR);
+        this.internal = String(num || 0);
+        this.as_int = as_integer(this.internal, DEC_SEP);
     }
 
     add = target => {
@@ -16,20 +16,20 @@ export class Precised {
         const smallest = operands[0].as_int.exp;
         const biggest = operands[1].as_int.exp;
 
-        const x = Number(format(operands[1].as_int.value, biggest - smallest, DECIMAL_SEPARATOR));
+        const x = Number(format(operands[1].as_int.value, biggest - smallest, DEC_SEP));
         const y = Number(operands[0].as_int.value);
 
         const result = String(x + y);
 
-        return format(result, smallest, DECIMAL_SEPARATOR);
+        return format(result, smallest, DEC_SEP);
     };
 
     sub = target => {
         return this.add(target * -1);
     };
 
-    precise = (precision = 0) => {
-        const tokens = this.internal.split(DECIMAL_SEPARATOR);
+    precise = (precision = 0, floatSep) => {
+        const tokens = this.internal.split(floatSep || DEC_SEP);
 
         if (tokens[1] !== undefined) {
             tokens[1] = tokens[1].slice(0, precision);
@@ -43,22 +43,22 @@ export class Precised {
             tokens[1] = addTrailingZeros(tokens[1], precision);
         }
 
-        return tokens[1] ? tokens.join(DECIMAL_SEPARATOR) : tokens[0];
+        return tokens[1] ? tokens.join(floatSep || DEC_SEP) : tokens[0];
     }
 
     format = (thousSep, floatSep) => {
-        const fs = thousSep === floatSep ? '.' : floatSep;
+        const fs = thousSep === floatSep ? DEC_SEP : floatSep;
         let res = this.internal;
     
         if (thousSep !== fs) {
             if (fs) {
-                res = String(res.replace('.', fs));
+                res = String(res.replace(DEC_SEP, fs));
             }
 
-            if ((thousSep && fs) || (thousSep && !fs && thousSep !== '.')) {
-                const tokens = res.split(fs || '.');
+            if ((thousSep && fs) || (thousSep && !fs && thousSep !== DEC_SEP)) {
+                const tokens = res.split(fs || DEC_SEP);
                 tokens[0] = tokens[0].replace(/\B(?=(\d{3})+(?!\d))/g, thousSep);
-                res = tokens.join(fs || '.');
+                res = tokens.join(fs || DEC_SEP);
             }
         }
 
